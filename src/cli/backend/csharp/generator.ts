e98bc764909243169cc08042c9edb5cfc023615d
MuriloDias12 <murilodiasdeoliveira@gmail.com>
2025-05-07 16:11:11 -0300

    REFACTOR : finish c# minimal api lib

--- src/cli/backend/csharp/generator.ts
+++ src/cli/backend/csharp/generator.ts
@@ -1,29 +1,25 @@
-import { Model } from "../../../language/generated/ast.js"
 import fs from "fs";
+import path from "path";
 
-import { generate as minimalApiGenerate } from "./minimal-api/generator.js";
-import { generate as minimalprojectGenerate} from "./minimal-api/project-generator.js"
-import { generate as minimalgenerateDocker } from "./minimal-api/docker-generator.js"
-import { generate as cleanArchGenerate} from "./clean-architecture/generator.js"
-import { generate as cleanprojectGenerate} from "./clean-architecture/project-generator.js"
-import { generate as cleangenerateDocker } from "./clean-architecture/docker-generator.js"
+import type { Model } from "spark-generators-lib";
+import { generators } from "spark-generators-lib";
 
-export function generate(model: Model, target_folder: string) : void {
-    const target_folder_back = target_folder+"/backend"
-    const target_folder_projname = target_folder_back + "/" + model.configuration?.name
+export function generate(model: Model, target_folder: string): void {
+  const target_folder_back = path.join(target_folder, "backend");
+  const target_folder_projname = path.join(target_folder_back, model.configuration?.name || "Projeto");
 
-    //creating folders
+  fs.mkdirSync(target_folder_back, { recursive: true });
 
-    fs.mkdirSync(target_folder_back, {recursive:true})
-    //minimal or clean-archi
-    if (model.configuration?.language == 'csharp-minimal-api'){
-        minimalApiGenerate(model,target_folder_projname )
-        minimalprojectGenerate(model, target_folder_back)
-        minimalgenerateDocker(model, target_folder_back)
-    }
-    else{
-        cleanArchGenerate(model, target_folder_projname)
-        cleanprojectGenerate(model, target_folder_back)
-        cleangenerateDocker(model, target_folder_back)
-    }
+  if (model.configuration?.language === "csharp-minimal-api") {
+    generators.minimal.minimalApiGenerate(model, target_folder_projname);
+    generators.minimal.minimalprojectGenerate(model, target_folder_back);
+    generators.minimal.minimalgenerateDocker(model, target_folder_back);
+  } else {
+    generators.clean.cleanArchGenerate(model, target_folder_projname);
+    generators.clean.cleanprojectGenerate(model, target_folder_back);
+    generators.clean.cleangenerateDocker(model, target_folder_back);
+  }
 }
+
+
+
