369245c8c924805b10ed1f88eb65a1a621a38115
brunoowcx <100306350+brunoowcx@users.noreply.github.com>
2025-05-09 22:23:34 -0300

    REFACTOR: added indexes in cleanarchitecture-lib #16

---
+++ spark-generators-lib/packages/generators/cleanArchitecture-generators/index.ts
@@ -0,0 +1,3 @@
+export { generate as cleanArchitecturegen } from './generator.js';
+export { generate as cleandockergen } from './docker-generator.js';
+export { generate as cleanprojectgen } from './project-generator.js';
--- spark-generators-lib/packages/generators/index.ts
+++ spark-generators-lib/packages/generators/index.ts
@@ -1,3 +1,4 @@
 export * as springboot from "./springboot.js"
 export * as django from "./django.js"
 export * as miniminal from "./minimal-API-generators/generator.js"
+export * as clean from "./cleanArchitecture-generators/generator.js"
