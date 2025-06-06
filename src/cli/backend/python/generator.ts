873766e2a811b7d61dd854e0e0a6202581e2102d
Arthur Valentim <valentimcaldeira100@gmail.com>
2025-06-06 17:08:25 -0300

    fix: ajuste no pyhton

--- src/cli/backend/python/generator.ts
+++ src/cli/backend/python/generator.ts
@@ -4,7 +4,7 @@ import { generators, Model as LibModel } from "spark-generators-lib";
 
 
 export function generate(model: LibModel.Model, target_folder: string): void {
-    // Não crie a pasta backend aqui, deixe para a lib!
+    
     fs.mkdirSync(target_folder, { recursive: true });
 
     generators.django.generate(model, target_folder);
