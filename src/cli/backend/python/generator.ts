6b66ddcf459d6df1da85a4a96fb5fb293b7c566b
Arthur Valentim <valentimcaldeira100@gmail.com>
2025-06-06 17:07:17 -0300

    fix: correção da implementação da lib no python

--- src/cli/backend/python/generator.ts
+++ src/cli/backend/python/generator.ts
@@ -1,21 +1,11 @@
-
 import fs from "fs";
 
-import { generators, Model as LibModel} from "spark-generators-lib"
-import { createPath } from "../../util/generator-utils.js";
-
-export function generate(model: LibModel.Model, target_folder: string) : void {
-
-    const target_folder_back = createPath(target_folder, "backend")
-
-    //creating folders
-    fs.mkdirSync(target_folder_back, {recursive:true})
+import { generators, Model as LibModel } from "spark-generators-lib";
 
 
-    generators.django.generate(model,target_folder_back)
+export function generate(model: LibModel.Model, target_folder: string): void {
+    // Não crie a pasta backend aqui, deixe para a lib!
+    fs.mkdirSync(target_folder, { recursive: true });
 
-
+    generators.django.generate(model, target_folder);
 }
-
-
-
